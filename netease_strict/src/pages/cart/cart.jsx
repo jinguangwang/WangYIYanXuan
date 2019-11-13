4a3415532fba5b97716e08931e0d8bace47e9e5b
qinbeibei <1769458871@qq.com>
2019-11-13 17:41:03 +0800

    '17:40'

--- netease_strict/src/pages/cart/cart.jsx
+++ netease_strict/src/pages/cart/cart.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import './cart.less'
 
 class cart extends React.Component {
 	render() {
 		return <div>
-		购物车
+			<div className="cart">
+				<span className="shop">购物车</span>
+				<span className="cartText">领劵</span>
+			</div>
+			<div className="cartDesc">
+				<ul className="desc">
+					<li>
+						<img src="http://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/servicePolicy-0ec842d966.png" alt="" />
+						<span>30天无忧退货</span>
+					</li>
+					<li>
+						<img src="http://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/servicePolicy-0ec842d966.png" alt="" />
+						<span>48小时快速退款</span>
+					</li>
+					<li>
+						<img src="http://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/servicePolicy-0ec842d966.png" alt="" />
+						<span>满88元免邮费</span>
+					</li>
+				</ul>
+			</div>
+			<div className="cartId">
+				<img src="http://yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/noCart-d6193bd6e4.png" alt=""/>
+				<div className="cartMessage">去添加点什么吧</div>
+				<button>登录</button>
+			</div>
+
 		</div>;
 	}
 }
